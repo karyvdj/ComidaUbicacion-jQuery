445273af5edc440d290cfb811ed47ffdd2f24933
karyvdj <kaina51@hotmail.com>
2017-06-05 14:48:37 -0500

    Version 0.1.0 completa, mostrar mapa con ubicacion y maquetacion

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+function initMap(){
+
+    var laboratoriaMexico = {lat: 19.4176437, lng: -99.167004};
+
+    var map = new google.maps.Map(document.getElementById('mapa'),{
+    zoom: 18,
+    center:laboratoriaMexico
+    });
+		
+		var marker = new google.maps.Marker({
+		 position:laboratoriaMexico,
+		 map: map
+	 });
+}
